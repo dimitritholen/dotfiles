b5c748d857b95d4f07da85b75851002d90327712
Dimitri Tholen <33127487+dimitritholen@users.noreply.github.com>
2022-01-21 22:58:46 +0100

    Added git switch

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,22 +2,34 @@ oh-my-posh --init --shell pwsh --config ~/M365Princess.omp.json | Invoke-Express
 
 function Get-GitStatus { & git status -sb $args }
 New-Alias -Name s -Value Get-GitStatus -Force -Option AllScope
-function Get-GitCommit { & git commit -vm $args }
+
+function Get-GitCommit { & git commit -v -m $args }
 New-Alias -Name c -Value Get-GitCommit -Force -Option AllScope
+
 function Get-GitAdd { & git add --all $args }
 New-Alias -Name ga -Value Get-GitAdd -Force -Option AllScope
+
 function Get-GitTree { & git log --graph --oneline --decorate $args }
 New-Alias -Name t -Value Get-GitTree -Force -Option AllScope
+
 function Get-GitPush { & git push $args }
 New-Alias -Name gps -Value Get-GitPush -Force -Option AllScope
+
 function Get-GitPull { & git pull $args }
 New-Alias -Name gpl -Value Get-GitPull -Force -Option AllScope
+
 function Get-GitFetch { & git fetch $args }
 New-Alias -Name f -Value Get-GitFetch -Force -Option AllScope
+
 function Get-GitCheckout { & git checkout $args }
 New-Alias -Name co -Value Get-GitCheckout -Force -Option AllScope
+
 function Get-GitBranch { & git branch $args }
 New-Alias -Name b -Value Get-GitBranch -Force -Option AllScope
+
+function Get-GitSwitch { & git switch $args }
+New-Alias -Name w -Value Get-GitSwitch -Force -Option AllScope
+
 function Get-GitRemote { & git remote -v $args }
 New-Alias -Name r -Value Get-GitRemote -Force -Option AllScope
 
@@ -31,6 +43,7 @@ git pull		gpl
 git fetch		f
 git checkout		co
 git branch		b
+git switch		w
 git remote -v		r
 "@
 
